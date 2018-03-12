919185c7aa2b7ac7b37112ec61036cb364f2394f
martinproxy0 <marcinsornek@gmail.com>
2018-03-12 23:38:40 +0100

    add hook and bugfix for error --> Unknown modifier: $pushAll --> { usePushEach: true }

--- server/models/lane.js
+++ server/models/lane.js
@@ -6,7 +6,13 @@ const laneSchema = new Schema({
   name: { type: 'String', required: true },
   notes: [{ type: Schema.ObjectId, ref: 'Note', required: true }],
   id: { type: 'String', required: true, unique: true },
-});
+},
+  { usePushEach: true }
+);
 
+laneSchema.pre('find', function (next) {
+  this.populate('notes');
+  next();
+});
 
 export default mongoose.model('Lane', laneSchema);
