beed04b78f97b794f088e0076e4f35954f97ebc5
martinproxy0 <marcinsornek@gmail.com>
2018-03-12 22:17:23 +0100

    CRUD fi lines is ready

---
+++ server/routes/lane.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import * as LaneController from '../controllers/lane.controller';
+
+const router = new Router();
+
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+// Delete a lane by laneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
+export default router;
